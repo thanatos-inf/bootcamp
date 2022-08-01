4e5909747f2809d88e80311e1d910bb0380157ca
thanatos_inf <thanatos_inf@mail.ru>
2022-08-02 01:42:27 +0300

    finally?

---
+++ 1_lesson/Program.cs
@@ -0,0 +1,18 @@
+﻿// Наибольший общий делитель (НОД) и Наименьшее общее кратное (НОК)
+
+int n = 140;
+int m = 175;
+int result = m * n;
+while (n != m)
+{
+    if (n > m)
+        n = n - m;
+    else
+        m = m - n;
+    
+    Console.WriteLine(n);
+    Console.WriteLine(m);
+    Console.WriteLine();
+}
+
+Console.WriteLine(result / m);
