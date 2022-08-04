bda549129e4925d50f6534d3928d1091891bbf5e
thanatos_inf <thanatos_inf@mail.ru>
2022-08-04 19:06:58 +0300

    4 les

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пузырьковая сортировка
+// Начальный массив: [3, 1, 5, 0, 7, 9, 8]
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значение массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+// Сортировка
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
+// Console.WriteLine("Отсортированный массив: [" + string.Join(", ", array) + "]");
