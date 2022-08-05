79012e868791056aa2841eae157acbf8192116b9
thanatos_inf <thanatos_inf@mail.ru>
2022-08-05 18:51:06 +0300

    les5

---
+++ 5_lesson/5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Быстрая сортировка (с использование рекурсии)
+
+int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
+QuickSort(arr, 0, arr.Length - 1);
+Console.Write($"Отсортированный массив: {string.Join(", ", arr)}");
+
+void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
