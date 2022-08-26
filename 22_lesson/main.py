aab59f9df485b3b50eeb4538f5ffe7f18b99f7c2
thanatos_inf <thanatos_inf@mail.ru>
2022-08-26 14:48:50 +0300

    some stuff added

--- 22_lesson/main.py
+++ 22_lesson/main.py
@@ -1,12 +1,33 @@
-from flask import Flask # Вытащил объект
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/<x>/<y>') # Создание главной страницы
-def main(x, y):
-    return str(int(x) + int(y)) # Convert.ToInt32("54");
+@app.route('/')
+def main():
+    return render_template('base.html', title='Главная страница')
 
 
-app.run(port=8080)
+@app.route('/listofsomething')
+def second():
+    
+    name = ['Укроп', 'Марина', 'Колесо', 'Боярышник', 'BitCoin']
+    return render_template('listofsomething.html', name=name,
+                             title='Произвольный список')
+
+
+@app.route('/portfolio')
+def third():
+    return render_template('portfolio.html', title='Контактные данные')
+    
+@app.route('/warning')
+def fourth():
+    return render_template('warning.html', title='ЗАЧЕМ?!')
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
